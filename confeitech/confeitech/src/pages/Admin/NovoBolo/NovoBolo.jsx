7b08c31cef6009782ae720542b7f75d5e6dc01ab
Antonio Gonçalves Sousa <antonio.sousa@sptech.school>
2024-10-10 01:44:59 -0300

    metade da tela de bolo feita

--- confeitech/confeitech/src/pages/Admin/NovoBolo/NovoBolo.jsx
+++ confeitech/confeitech/src/pages/Admin/NovoBolo/NovoBolo.jsx
@@ -1,16 +1,26 @@
-import React,{useEffect} from "react";
+import React, { useEffect } from "react";
 import styles from "./NovoBolo.module.css";
 import NavBarAdmin from "../../../components/NavBarAdmin/NavBarAdmin";
 
 const NovoBolo = () => {
-    return(
+    return (
         <div className={styles["body"]}>
             <NavBarAdmin />
             <div className={styles["newCake"]}>
                 <div className={styles["boxNew"]}>
-                    <div className={styles["titleCake"]}>
-                        <h1>Titulo</h1>
-                        <h3>Modificar Nome +</h3>
+                    <div className={styles["contentCake"]}>
+                        <div className={styles["titleCake"]}>
+                            <h1>Titulo</h1>
+                            <h3 className={styles["link"]}>Modificar Nome +</h3>
+                        </div>
+                        <div className={styles["midCake"]}>
+                            <input type="text" className={styles["inputCake"]} />
+                            <h3 className={styles["link"]}>Modificar Descrição +</h3>
+                        </div>
+                        <div className={styles["titleCake"]}>
+                            <h1 className={styles["priceCake"]}>R$: 00,00</h1>
+                            <h3 className={styles["link"]}>Modificar Preço +</h3>
+                        </div>
                     </div>
                 </div>
             </div>
