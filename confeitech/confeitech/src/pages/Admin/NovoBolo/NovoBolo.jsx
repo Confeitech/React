c8b9528328f2c5a03663377cad1ceb1239703133
Antonio Gonçalves Sousa <antonio.sousa@sptech.school>
2024-10-14 13:52:22 -0300

    Tela de novo bolo praticamente terminada, só faltando imagem de concluir

--- confeitech/confeitech/src/pages/Admin/NovoBolo/NovoBolo.jsx
+++ confeitech/confeitech/src/pages/Admin/NovoBolo/NovoBolo.jsx
@@ -1,8 +1,10 @@
-import React, { useEffect } from "react";
+import React, { useState } from "react";
 import styles from "./NovoBolo.module.css";
 import NavBarAdmin from "../../../components/NavBarAdmin/NavBarAdmin";
 
+
 const NovoBolo = () => {
+    const [number, setNumber] = useState(0);
     return (
         <div className={styles["body"]}>
             <NavBarAdmin />
@@ -10,16 +12,121 @@ const NovoBolo = () => {
                 <div className={styles["boxNew"]}>
                     <div className={styles["contentCake"]}>
                         <div className={styles["titleCake"]}>
-                            <h1>Titulo</h1>
+                            <h2>Titulo</h2>
                             <h3 className={styles["link"]}>Modificar Nome +</h3>
                         </div>
                         <div className={styles["midCake"]}>
-                            <input type="text" className={styles["inputCake"]} />
+                            <textarea placeholder="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique harum a pariatur quo. Blanditiis facilis quod maiores a architecto laudantium eligendi." type="text" className={styles["inputCake"]} />
                             <h3 className={styles["link"]}>Modificar Descrição +</h3>
                         </div>
                         <div className={styles["titleCake"]}>
-                            <h1 className={styles["priceCake"]}>R$: 00,00</h1>
+                            <h2 className={styles["priceCake"]}>R$: 00,00</h2>
                             <h3 className={styles["link"]}>Modificar Preço +</h3>
+                        </div>
+                        <div className={styles["buttonsCake"]}>
+                            <button className={styles["buttonPurple"]}>1,0kg</button>
+                            <button className={styles["buttonWhite"]}>+</button>
+                        </div>
+                        <div className={styles["tabelaCake"]}>
+                            <div className={styles["card"]}>
+                                <div className={styles["detailsCard"]}>
+                                    <h3>morangos</h3>
+                                    <h3 style={{color : "#1B5519"}}>+ R$0,00</h3>
+                                </div>
+                                <div className={styles["buttonsCard"]}>
+                                    <button>-</button>
+                                    <div className={styles["number"]}>
+                                        <h3>
+                                            {number}
+                                        </h3>
+                                    </div>
+                                    <button>+</button>
+                                </div>
+                                <div className={styles["meio"]}>
+                                    <h3 className={styles["link"]}>Modificar +</h3>
+                                </div>
+                            </div>
+                            <div className={styles["card"]}>
+                                <div className={styles["detailsCard"]}>
+                                    <h3>morangos</h3>
+                                    <h3 style={{color : "#1B5519"}}>+ R$0,00</h3>
+                                </div>
+                                <div className={styles["buttonsCard"]}>
+                                    <button>-</button>
+                                    <div className={styles["number"]}>
+                                        <h3>
+                                            {number}
+                                        </h3>
+                                    </div>
+                                    <button>+</button>
+                                </div>
+                                <div className={styles["meio"]}>
+                                    <h3 className={styles["link"]}>Modificar +</h3>
+                                </div>
+                            </div>
+                            <div className={styles["card"]}>
+                                <div className={styles["detailsCard"]}>
+                                    <h3>morangos</h3>
+                                    <h3 style={{color : "#1B5519"}}>+ R$0,00</h3>
+                                </div>
+                                <div className={styles["buttonsCard"]}>
+                                    <button>-</button>
+                                    <div className={styles["number"]}>
+                                        <h3>
+                                            {number}
+                                        </h3>
+                                    </div>
+                                    <button>+</button>
+                                </div>
+                                <div className={styles["meio"]}>
+                                    <h3 className={styles["link"]}>Modificar +</h3>
+                                </div>
+                            </div>
+                            <div className={styles["card"]}>
+                                <div className={styles["detailsCard"]}>
+                                    <h3>morangos</h3>
+                                    <h3 style={{color : "#1B5519"}}>+ R$0,00</h3>
+                                </div>
+                                <div className={styles["buttonsCard"]}>
+                                    <button>-</button>
+                                    <div className={styles["number"]}>
+                                        <h3>
+                                            {number}
+                                        </h3>
+                                    </div>
+                                    <button>+</button>
+                                </div>
+                                <div className={styles["meio"]}>
+                                    <h3 className={styles["link"]}>Modificar +</h3>
+                                </div>
+                            </div>
+                            <div className={styles["card"]}>
+                                <div className={styles["detailsCard"]}>
+                                    <h3>morangos</h3>
+                                    <h3 style={{color : "#1B5519"}}>+ R$0,00</h3>
+                                </div>
+                                <div className={styles["buttonsCard"]}>
+                                    <button>-</button>
+                                    <div className={styles["number"]}>
+                                        <h3>
+                                            {number}
+                                        </h3>
+                                    </div>
+                                    <button>+</button>
+                                </div>
+                                <div className={styles["meio"]}>
+                                    <h3 className={styles["link"]}>Modificar +</h3>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={styles["buttonsSpace"]}>
+                            <button className={styles["buttonAdd"]}>Adicionar+</button>
+                            <div className={styles["correct"]}>
+                                <button className={styles["addImage"]}>Adicionar imagem</button>
+                            </div>
+                        </div>
+                        <div className={styles["check"]}>
+                            
                         </div>
                     </div>
                 </div>
