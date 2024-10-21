f2b4050d8c0adeedee5e89ddcd3a88755fec66bc
yasmi-oliveira <yasmin.silva@sptech.school>
2024-10-21 20:12:29 -0300

    estilizando fron-end ADM

--- confeitech/confeitech/src/components/CardEncomendaAceita/CardEncomendaAceita.jsx
+++ confeitech/confeitech/src/components/CardEncomendaAceita/CardEncomendaAceita.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import styles from "./CardEncomendaAceita.module.css";
+import imgBolo from "../../utils/assets/fatia-de-bolo-de-chocolate-recheado-com-creme-marrom-e-morango-por-cima_993044-36.avif"
+import imgBolo2 from "../../utils/assets/bolo-com-dois-recheios-em-pedaço.jpg.webp"
 
 const CardEncomendaAceita = () => {
     const [openModal, setOpenModal] = useState(false);
@@ -37,28 +39,74 @@ const CardEncomendaAceita = () => {
     };
 
     return (
-        <div className={styles["card"]}>
-            <div className={styles["image"]}>
-                <img src="" alt="Bolo de chocolate" />
-            </div>
-            <div className={styles["info"]}>
-                <h3 className={styles["h3_cardEncomenda"]}>Bolo de chocolate</h3>
-                <div className={styles["description"]}>
-                    <h5 className={styles["h5_cardEncomenda"]}>Cliente: João da Silva</h5>
-                    <h5 className={styles["h5_cardEncomenda"]}>Descrição: Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae aliquam pariatur voluptates ea molestias.</h5>
-                    <h5 className={styles["h5_cardEncomenda"]}>Data do pedido: 25/12/2025</h5>
-                    <h5 className={styles["h5_cardEncomendaRed"]}>PREVISTO PARA RETIRADA: : 25/12/2025</h5>
+        <>
+            <div className={styles["card"]}>
+                <div className={styles["image"]}>
+                    <img className={styles["imagemBolo"]} src={imgBolo} alt="Bolo de chocolate" />
                 </div>
+                <div className={styles["info"]}>
+                    <h3 className={styles["h3_cardEncomenda"]}>Bolo de chocolate</h3>
+                    <div className={styles["description"]}>
+                        <h5 className={styles["h5_cardEncomenda"]}>Cliente: João da Silva</h5>
+                        <h5 className={styles["h5_cardEncomenda"]}>Descrição: Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae aliquam pariatur voluptates ea molestias.</h5>
+                        <h5 className={styles["h5_cardEncomenda"]}>Data do pedido: 25/12/2025</h5>
+                        <h5 className={styles["h5_cardEncomendaRed"]}>PREVISTO PARA RETIRADA: : 25/12/2025</h5>
+                    </div>
+                </div>
+                <div className={styles["edit"]}>
+                    <h3 className={styles["h3_edit"]}>R$ 89,90</h3>
+                    <h3 className={styles["status"]} style={{ color: colors[indiceCor] }}>{status}</h3>
+                    <span className={styles["span_button"]}>
+                        <button className={styles["buttonEdit"]} onClick={() => setOpenModal(true)}>Alterar status</button>
+                    </span>
+                </div>
+                <Dialog isOpen={openModal} />
             </div>
-            <div className={styles["edit"]}>
-                <h3 className={styles["h3_edit"]}>R$ 89,90</h3>
-                <h3 className={styles["status"]} style={{ color: colors[indiceCor] }}>{status}</h3>
-                <span className={styles["span_button"]}>
-                    <button className={styles["buttonEdit"]} onClick={() => setOpenModal(true)}>Alterar status</button>
-                </span>
+            <div className={styles["card"]}>
+                <div className={styles["image"]}>
+                <img className={styles["imagemBolo"]} src={imgBolo2} alt="Bolo de chocolate" />
+                </div>
+                <div className={styles["info"]}>
+                    <h3 className={styles["h3_cardEncomenda"]}>Bolo de chocolate</h3>
+                    <div className={styles["description"]}>
+                        <h5 className={styles["h5_cardEncomenda"]}>Cliente: João da Silva</h5>
+                        <h5 className={styles["h5_cardEncomenda"]}>Descrição: Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae aliquam pariatur voluptates ea molestias.</h5>
+                        <h5 className={styles["h5_cardEncomenda"]}>Data do pedido: 25/12/2025</h5>
+                        <h5 className={styles["h5_cardEncomendaRed"]}>PREVISTO PARA RETIRADA: : 25/12/2025</h5>
+                    </div>
+                </div>
+                <div className={styles["edit"]}>
+                    <h3 className={styles["h3_edit"]}>R$ 89,90</h3>
+                    <h3 className={styles["status"]} style={{ color: colors[indiceCor] }}>{status}</h3>
+                    <span className={styles["span_button"]}>
+                        <button className={styles["buttonEdit"]} onClick={() => setOpenModal(true)}>Alterar status</button>
+                    </span>
+                </div>
+                <Dialog isOpen={openModal} />
             </div>
-            <Dialog isOpen={openModal} />
-        </div>
+            <div className={styles["card"]}>
+                <div className={styles["image"]}>
+                    <img src="" alt="Bolo de chocolate" />
+                </div>
+                <div className={styles["info"]}>
+                    <h3 className={styles["h3_cardEncomenda"]}>Bolo de chocolate</h3>
+                    <div className={styles["description"]}>
+                        <h5 className={styles["h5_cardEncomenda"]}>Cliente: João da Silva</h5>
+                        <h5 className={styles["h5_cardEncomenda"]}>Descrição: Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae aliquam pariatur voluptates ea molestias.</h5>
+                        <h5 className={styles["h5_cardEncomenda"]}>Data do pedido: 25/12/2025</h5>
+                        <h5 className={styles["h5_cardEncomendaRed"]}>PREVISTO PARA RETIRADA: : 25/12/2025</h5>
+                    </div>
+                </div>
+                <div className={styles["edit"]}>
+                    <h3 className={styles["h3_edit"]}>R$ 89,90</h3>
+                    <h3 className={styles["status"]} style={{ color: colors[indiceCor] }}>{status}</h3>
+                    <span className={styles["span_button"]}>
+                        <button className={styles["buttonEdit"]} onClick={() => setOpenModal(true)}>Alterar status</button>
+                    </span>
+                </div>
+                <Dialog isOpen={openModal} />
+            </div>
+        </>
     );
 };
 
