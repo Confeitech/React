323d52f3ee91f95ad1f68d614701ce36d4669571
Antonio Gonçalves Sousa <antonio.sousa@sptech.school>
2024-10-28 13:24:04 -0300

    Adicionando botões na tabela de ingredientes e alterando as cores dos botões para padronização

--- confeitech/confeitech/src/components/CardEncomendaAceita/CardEncomendaAceita.jsx
+++ confeitech/confeitech/src/components/CardEncomendaAceita/CardEncomendaAceita.jsx
@@ -1,113 +1,188 @@
 import React, { useState } from "react";
 import styles from "./CardEncomendaAceita.module.css";
-import imgBolo from "../../utils/assets/fatia-de-bolo-de-chocolate-recheado-com-creme-marrom-e-morango-por-cima_993044-36.avif"
-import imgBolo2 from "../../utils/assets/bolo-com-dois-recheios-em-pedaço.jpg.webp"
+import imgBolo from "../../utils/assets/fatia-de-bolo-de-chocolate-recheado-com-creme-marrom-e-morango-por-cima_993044-36.avif";
+import imgBolo2 from "../../utils/assets/bolo-com-dois-recheios-em-pedaço.jpg.webp";
 
 const CardEncomendaAceita = () => {
-    const [openModal, setOpenModal] = useState(false);
+  const [openModal, setOpenModal] = useState(false);
 
-    const [status, setStatus] = useState("Em Andamento");
-    const [indiceCor, setIndiceCor] = useState(1);
-    const colors = ["#5CE45C", "#B89300", "#000", "#FF0000"];
+  const [status, setStatus] = useState("Em Andamento");
+  const [indiceCor, setIndiceCor] = useState(1);
+  const colors = ["#5CE45C", "#B89300", "#000", "#FF0000"];
 
-    const handleStatus = (numero) => {
-        if (numero === 1) setStatus("Concluido");
-        else if (numero === 2) setStatus("Em Andamento");
-        else if (numero === 3) setStatus("Pendente");
-        else if (numero === 4) setStatus("Pedido Cancelado");
-        setIndiceCor(numero - 1);
-        setOpenModal(false);
-    };
+  const handleStatus = (numero) => {
+    if (numero === 1) setStatus("Concluido");
+    else if (numero === 2) setStatus("Em Andamento");
+    else if (numero === 3) setStatus("Pendente");
+    else if (numero === 4) setStatus("Pedido Cancelado");
+    setIndiceCor(numero - 1);
+    setOpenModal(false);
+  };
 
-    const Dialog = ({ isOpen }) => {
-        if (isOpen) {
-            return (
-                <>
-                    <div className={styles["backModal"]}>
-                    </div>
-                    <div className={styles["contentModal"]}>
-                        <h2 className={styles["h2_status"]}>Alterar Status</h2>
-                        <button className={styles["buttonConcluidoModal"]} onClick={() => handleStatus(1)}>Concluido</button>
-                        <button className={styles["buttonPreparacaoModal"]} onClick={() => handleStatus(2)}>Em Andamento</button>
-                        <button className={styles["buttonPendenteModal"]} onClick={() => handleStatus(3)}>Pendente</button>
-                        <button className={styles["buttonCancelarModal"]} onClick={() => handleStatus(4)}>Cancelar Pedido</button>
-                    </div>
-                </>
-            );
-        }
-        return null;
-    };
-
-    return (
+  const Dialog = ({ isOpen }) => {
+    if (isOpen) {
+      return (
         <>
-            <div className={styles["card"]}>
-                <div className={styles["image"]}>
-                    <img className={styles["imagemBolo"]} src={imgBolo} alt="Bolo de chocolate" />
-                </div>
-                <div className={styles["info"]}>
-                    <h3 className={styles["h3_cardEncomenda"]}>Bolo de chocolate</h3>
-                    <div className={styles["description"]}>
-                        <h5 className={styles["h5_cardEncomenda"]}>Cliente: João da Silva</h5>
-                        <h5 className={styles["h5_cardEncomenda"]}>Descrição: Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae aliquam pariatur voluptates ea molestias.</h5>
-                        <h5 className={styles["h5_cardEncomenda"]}>Data do pedido: 25/12/2025</h5>
-                        <h5 className={styles["h5_cardEncomendaRed"]}>PREVISTO PARA RETIRADA: : 25/12/2025</h5>
-                    </div>
-                </div>
-                <div className={styles["edit"]}>
-                    <h3 className={styles["h3_edit"]}>R$ 89,90</h3>
-                    <h3 className={styles["status"]} style={{ color: colors[indiceCor] }}>{status}</h3>
-                    <span className={styles["span_button"]}>
-                        <button className={styles["buttonEdit"]} onClick={() => setOpenModal(true)}>Alterar status</button>
-                    </span>
-                </div>
-                <Dialog isOpen={openModal} />
-            </div>
-            <div className={styles["card"]}>
-                <div className={styles["image"]}>
-                <img className={styles["imagemBolo"]} src={imgBolo2} alt="Bolo de chocolate" />
-                </div>
-                <div className={styles["info"]}>
-                    <h3 className={styles["h3_cardEncomenda"]}>Bolo de chocolate</h3>
-                    <div className={styles["description"]}>
-                        <h5 className={styles["h5_cardEncomenda"]}>Cliente: João da Silva</h5>
-                        <h5 className={styles["h5_cardEncomenda"]}>Descrição: Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae aliquam pariatur voluptates ea molestias.</h5>
-                        <h5 className={styles["h5_cardEncomenda"]}>Data do pedido: 25/12/2025</h5>
-                        <h5 className={styles["h5_cardEncomendaRed"]}>PREVISTO PARA RETIRADA: : 25/12/2025</h5>
-                    </div>
-                </div>
-                <div className={styles["edit"]}>
-                    <h3 className={styles["h3_edit"]}>R$ 89,90</h3>
-                    <h3 className={styles["status"]} style={{ color: colors[indiceCor] }}>{status}</h3>
-                    <span className={styles["span_button"]}>
-                        <button className={styles["buttonEdit"]} onClick={() => setOpenModal(true)}>Alterar status</button>
-                    </span>
-                </div>
-                <Dialog isOpen={openModal} />
-            </div>
-            <div className={styles["card"]}>
-                <div className={styles["image"]}>
-                    <img src="" alt="Bolo de chocolate" />
-                </div>
-                <div className={styles["info"]}>
-                    <h3 className={styles["h3_cardEncomenda"]}>Bolo de chocolate</h3>
-                    <div className={styles["description"]}>
-                        <h5 className={styles["h5_cardEncomenda"]}>Cliente: João da Silva</h5>
-                        <h5 className={styles["h5_cardEncomenda"]}>Descrição: Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae aliquam pariatur voluptates ea molestias.</h5>
-                        <h5 className={styles["h5_cardEncomenda"]}>Data do pedido: 25/12/2025</h5>
-                        <h5 className={styles["h5_cardEncomendaRed"]}>PREVISTO PARA RETIRADA: : 25/12/2025</h5>
-                    </div>
-                </div>
-                <div className={styles["edit"]}>
-                    <h3 className={styles["h3_edit"]}>R$ 89,90</h3>
-                    <h3 className={styles["status"]} style={{ color: colors[indiceCor] }}>{status}</h3>
-                    <span className={styles["span_button"]}>
-                        <button className={styles["buttonEdit"]} onClick={() => setOpenModal(true)}>Alterar status</button>
-                    </span>
-                </div>
-                <Dialog isOpen={openModal} />
-            </div>
+          <div className={styles["backModal"]}></div>
+          <div className={styles["contentModal"]}>
+            <h2 className={styles["h2_status"]}>Alterar Status</h2>
+            <button
+              className={styles["buttonConcluidoModal"]}
+              onClick={() => handleStatus(1)}
+            >
+              Concluido
+            </button>
+            <button
+              className={styles["buttonPreparacaoModal"]}
+              onClick={() => handleStatus(2)}
+            >
+              Em Andamento
+            </button>
+            <button
+              className={styles["buttonPendenteModal"]}
+              onClick={() => handleStatus(3)}
+            >
+              Pendente
+            </button>
+            <button
+              className={styles["buttonCancelarModal"]}
+              onClick={() => handleStatus(4)}
+            >
+              Cancelar Pedido
+            </button>
+          </div>
         </>
-    );
+      );
+    }
+    return null;
+  };
+
+  return (
+    <>
+      <div className={styles["card"]}>
+        <div className={styles["image"]}>
+          <img
+            className={styles["imagemBolo"]}
+            src={imgBolo}
+            alt="Bolo de chocolate"
+          />
+        </div>
+        <div className={styles["info"]}>
+          <h3 className={styles["h3_cardEncomenda"]}>Bolo de chocolate</h3>
+          <div className={styles["description"]}>
+            <h5 className={styles["h5_cardEncomenda"]}>
+              Cliente: João da Silva
+            </h5>
+            <h5 className={styles["h5_cardEncomenda"]}>
+              Descrição: Lorem ipsum dolor sit amet consectetur, adipisicing
+              elit. Molestiae aliquam pariatur voluptates ea molestias.
+            </h5>
+            <h5 className={styles["h5_cardEncomenda"]}>
+              Data do pedido: 25/12/2025
+            </h5>
+            <h5 className={styles["h5_cardEncomendaRed"]}>
+              PREVISTO PARA RETIRADA: : 25/12/2025
+            </h5>
+          </div>
+        </div>
+        <div className={styles["edit"]}>
+          <h3 className={styles["h3_edit"]}>R$ 89,90</h3>
+          <h3 className={styles["status"]} style={{ color: colors[indiceCor] }}>
+            {status}
+          </h3>
+          <span className={styles["span_button"]}>
+            <button
+              className={styles["buttonEdit"]}
+              onClick={() => setOpenModal(true)}
+            >
+              Alterar status
+            </button>
+          </span>
+        </div>
+        <Dialog isOpen={openModal} />
+      </div>
+      <div className={styles["card"]}>
+        <div className={styles["image"]}>
+          <img
+            className={styles["imagemBolo"]}
+            src={imgBolo2}
+            alt="Bolo de chocolate"
+          />
+        </div>
+        <div className={styles["info"]}>
+          <h3 className={styles["h3_cardEncomenda"]}>Bolo de chocolate</h3>
+          <div className={styles["description"]}>
+            <h5 className={styles["h5_cardEncomenda"]}>
+              Cliente: João da Silva
+            </h5>
+            <h5 className={styles["h5_cardEncomenda"]}>
+              Descrição: Lorem ipsum dolor sit amet consectetur, adipisicing
+              elit. Molestiae aliquam pariatur voluptates ea molestias.
+            </h5>
+            <h5 className={styles["h5_cardEncomenda"]}>
+              Data do pedido: 25/12/2025
+            </h5>
+            <h5 className={styles["h5_cardEncomendaRed"]}>
+              PREVISTO PARA RETIRADA: : 25/12/2025
+            </h5>
+          </div>
+        </div>
+        <div className={styles["edit"]}>
+          <h3 className={styles["h3_edit"]}>R$ 89,90</h3>
+          <h3 className={styles["status"]} style={{ color: colors[indiceCor] }}>
+            {status}
+          </h3>
+          <span className={styles["span_button"]}>
+            <button
+              className={styles["buttonEdit"]}
+              onClick={() => setOpenModal(true)}
+            >
+              Alterar status
+            </button>
+          </span>
+        </div>
+        <Dialog isOpen={openModal} />
+      </div>
+      <div className={styles["card"]}>
+        <div className={styles["image"]}>
+          <img src="" alt="Bolo de chocolate" />
+        </div>
+        <div className={styles["info"]}>
+          <h3 className={styles["h3_cardEncomenda"]}>Bolo de chocolate</h3>
+          <div className={styles["description"]}>
+            <h5 className={styles["h5_cardEncomenda"]}>
+              Cliente: João da Silva
+            </h5>
+            <h5 className={styles["h5_cardEncomenda"]}>
+              Descrição: Lorem ipsum dolor sit amet consectetur, adipisicing
+              elit. Molestiae aliquam pariatur voluptates ea molestias.
+            </h5>
+            <h5 className={styles["h5_cardEncomenda"]}>
+              Data do pedido: 25/12/2025
+            </h5>
+            <h5 className={styles["h5_cardEncomendaRed"]}>
+              PREVISTO PARA RETIRADA: : 25/12/2025
+            </h5>
+          </div>
+        </div>
+        <div className={styles["edit"]}>
+          <h3 className={styles["h3_edit"]}>R$ 89,90</h3>
+          <h3 className={styles["status"]} style={{ color: colors[indiceCor] }}>
+            {status}
+          </h3>
+          <span className={styles["span_button"]}>
+            <button
+              className={styles["buttonEdit"]}
+              onClick={() => setOpenModal(true)}
+            >
+              Alterar status
+            </button>
+          </span>
+        </div>
+        <Dialog isOpen={openModal} />
+      </div>
+    </>
+  );
 };
 
 export default CardEncomendaAceita;
--- confeitech/confeitech/src/pages/Admin/Ingredientes/Ingredientes.jsx
+++ confeitech/confeitech/src/pages/Admin/Ingredientes/Ingredientes.jsx
@@ -1,46 +1,67 @@
 import React from "react";
 import styles from "./Ingredientes.module.css";
 import NavBarAdmin from "../../../components/NavBarAdmin/NavBarAdmin";
+import excluir from "../../../utils/assets/excluir.png";
+import editar from "../../../utils/assets/editar.png";
 
 const Ingredientes = () => {
-    const estiloTabela = {textAlign: "center", height: "50px", border: "1px solid #000"};
-    const estiloTituloTabela = {backgroundColor: "#D9D9D9", height: "25px"};
-    return (
-        <div className={styles["body"]}>
-            <NavBarAdmin />
-            <div className={styles["content"]}>
-                <div className={styles["box"]}>
-                    <h1>Adicionais</h1>
-                    <div className={styles["space"]}>
-                        <table className={styles["tabela"]}>
-                            <thead style={estiloTituloTabela}>
-                                <tr>
-                                    <th>Adicional</th>
-                                    <th>Valor</th>
-                                    <th>Peso</th>
-                                    <th>Ações</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td style={estiloTabela}>Paçoca</td>
-                                    <td style={estiloTabela}>R$ 0,00</td>
-                                    <td style={estiloTabela}>0 gramas</td>
-                                    <td style={estiloTabela}>Excluir</td>
-                                </tr>
-                                <tr>
-                                    <td style={estiloTabela}>Paçoca</td>
-                                    <td style={estiloTabela}>R$ 0,00</td>
-                                    <td style={estiloTabela}>0 gramas</td>
-                                    <td style={estiloTabela}>Excluir</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
+  const estiloTabela = {
+    textAlign: "center",
+    height: "50px",
+    border: "1px solid #000",
+  };
+  const estiloTituloTabela = {
+    backgroundColor: "#D9D9D9",
+    height: "25px",
+  };
+  const estiloBotao = {
+    width: "25px",
+    height: "25px",
+    margin: "5px",
+    cursor: "pointer",
+  };
+  return (
+    <div className={styles["body"]}>
+      <NavBarAdmin />
+      <div className={styles["content"]}>
+        <div className={styles["box"]}>
+          <h1>Adicionais</h1>
+          <div className={styles["space"]}>
+            <table className={styles["tabela"]}>
+              <thead style={estiloTituloTabela}>
+                <tr>
+                  <th>Adicional</th>
+                  <th>Valor</th>
+                  <th>Peso</th>
+                  <th>Ações</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td style={estiloTabela}>Paçoca</td>
+                  <td style={estiloTabela}>R$ 0,00</td>
+                  <td style={estiloTabela}>0 gramas</td>
+                  <td style={estiloTabela}>
+                    <img src={excluir} alt="Excluir" style={estiloBotao} />
+                    <img src={editar} alt="Editar" style={estiloBotao} />
+                  </td>
+                </tr>
+                <tr>
+                  <td style={estiloTabela}>Paçoca</td>
+                  <td style={estiloTabela}>R$ 0,00</td>
+                  <td style={estiloTabela}>0 gramas</td>
+                  <td style={estiloTabela}>
+                    <img src={excluir} alt="Excluir" style={estiloBotao} />
+                    <img src={editar} alt="Editar" style={estiloBotao} />
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Ingredientes;
