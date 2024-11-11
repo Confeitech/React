20f17363abd843153de7e591359da9598635bf55
Antonio Gonçalves Sousa <antonio.sousa@sptech.school>
2024-11-11 17:52:57 -0300

    Fazendo chamadas api para edição de bolos nos cardCardapio

--- confeitech/confeitech/src/components/CardCardapio/CardCardapio.jsx
+++ confeitech/confeitech/src/components/CardCardapio/CardCardapio.jsx
@@ -1,41 +1,43 @@
 import React from "react";
 import styles from "./CardCardapio.module.css";
 import { useNavigate } from "react-router-dom";
-import imgBolo from "../../utils/assets/fatia-de-bolo-de-chocolate-recheado-com-creme-marrom-e-morango-por-cima_993044-36.avif"
+import imgBolo from "../../utils/assets/fatia-de-bolo-de-chocolate-recheado-com-creme-marrom-e-morango-por-cima_993044-36.avif";
 
-const CardCardapio = (
-    { nome,
-        descricao,
-        preco }
-) => {
-    const navigate = useNavigate(); // Hook do React Router para navegação
+const CardCardapio = ({ index, nome, descricao, preco }) => {
+  const navigate = useNavigate(); // Hook do React Router para navegação
 
-    const handleEditCakeClick = () => {
-        navigate('/modificar-bolo'); // Navega para a página /about
-    };
+  const handleEditCakeClick = (index) => {
+    sessionStorage.setItem("index", index);
+    console.log("index", index);
+    navigate("/modificar-bolo"); // Navega para a página /about
+  };
 
-    return (
-        <div className={styles["row"]}>
-            <div className={styles["card"]}>
-                <div className={styles["imagem"]}>
-                    <img className={styles["imagemBolo"]} src={imgBolo} alt="Bolo de chocolate" />
-                </div>
-                <div className={styles["details"]}>
-                    <h3 className={styles["h3_cardCardapio"]}>
-                        {nome || "N\A"}
-                    </h3>
-                    <h5 className={styles["h5_cardCardapio"]}>
-                        {descricao || "N\A"}
-                    </h5>
-                </div>
-                <div className={styles["price"]}>
-                    <h2>R$ {preco || "N\A"}
-                    </h2>
-                    <button className={styles["buttonEdit"]} onClick={handleEditCakeClick}>Modificar Bolo</button>
-                </div>
-            </div>
+  return (
+    <div className={styles["row"]}>
+      <div className={styles["card"]}>
+        <div className={styles["imagem"]}>
+          <img
+            className={styles["imagemBolo"]}
+            src={imgBolo}
+            alt="Bolo de chocolate"
+          />
         </div>
-    );
+        <div className={styles["details"]}>
+          <h3 className={styles["h3_cardCardapio"]}>{nome || "NA"}</h3>
+          <h5 className={styles["h5_cardCardapio"]}>{descricao || "NA"}</h5>
+        </div>
+        <div className={styles["price"]}>
+          <h2>R$ {preco || "NA"}</h2>
+          <button
+            className={styles["buttonEdit"]}
+            onClick={() =>handleEditCakeClick(index)}
+          >
+            Modificar Bolo
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CardCardapio;
--- confeitech/confeitech/src/pages/Admin/ModificarBolo/ModificarBolo.jsx
+++ confeitech/confeitech/src/pages/Admin/ModificarBolo/ModificarBolo.jsx
@@ -1,154 +1,146 @@
-import { React, useEffect } from "react";
+import { React, useState, useEffect } from "react";
 import styles from "./ModificarBolo.module.css";
 import NavBarAdmin from "../../../components/NavBarAdmin/NavBarAdmin";
 import check from "../../../utils/assets/verifica.png";
 import cancel from "../../../utils/assets/cancelar.png";
+import api from "../../../api";
+import Adicionais from "../../../components/rowTabelaAdicionais/Adicionais";
+
 
 const ModificarBolo = () => {
-    const number = 0;
-    return (
-        <div className={styles["corpo"]}>
-            <NavBarAdmin />
-            <div className={styles["editCake"]}>
-                <div className={styles["editLeft"]}>
-                    <div className={styles["contentEditLeft"]}>
-                        <h3 className={styles["link"]}>Modificar imagem +</h3>
-                        <div className={styles["editImage"]}></div>
-                    </div>
-                </div>
-                <div className={styles["editRight"]}>
-                    <div className={styles["boxEditRight"]}>
-                        <div className={styles["contentEditRight"]}>
-                            <div className={styles["titleEdit"]}>
-                                <h2>Titulo</h2>
-                                <h3 className={styles["link"]}>Modificar Nome +</h3>
-                            </div>
-                            <div className={styles["midEdit"]}>
-                                <textarea placeholder="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique harum a pariatur quo." type="text" className={styles["inputEdit"]} />
-                                <h3 className={styles["link"]}>Modificar Descrição +</h3>
-                            </div>
-                            <div className={styles["titleEdit"]}>
-                                <h2 className={styles["priceEdit"]}>R$: 00,00</h2>
-                                <h3 className={styles["link"]}>Modificar Preço +</h3>
-                            </div>
-                            <div className={styles["buttonsEdit"]}>
-                                <button className={styles["buttonPurple"]}>1,0kg</button>
-                                <button className={styles["buttonPurple"]}>1,0kg</button>
-                                <button className={styles["buttonPurple"]}>1,0kg</button>
-                                <button className={styles["buttonWhite"]}>+</button>
-                            </div>
-                            <div className={styles["tabelaEdit"]}>
-                                <div className={styles["card"]}>
-                                    <div className={styles["detailsCard"]}>
-                                        <h3>morangos</h3>
-                                        <h3 style={{ color: "#1B5519" }}>+ R$0,00</h3>
-                                    </div>
-                                    <div className={styles["buttonsCard"]}>
-                                        <button>-</button>
-                                        <div className={styles["number"]}>
-                                            <h3>
-                                                {number}
-                                            </h3>
-                                        </div>
-                                        <button>+</button>
-                                    </div>
-                                    <div className={styles["meio"]}>
-                                        <h3 className={styles["link"]}>Modificar +</h3>
-                                    </div>
-                                </div>
-                                <div className={styles["card"]}>
-                                    <div className={styles["detailsCard"]}>
-                                        <h3>morangos</h3>
-                                        <h3 style={{ color: "#1B5519" }}>+ R$0,00</h3>
-                                    </div>
-                                    <div className={styles["buttonsCard"]}>
-                                        <button>-</button>
-                                        <div className={styles["number"]}>
-                                            <h3>
-                                                {number}
-                                            </h3>
-                                        </div>
-                                        <button>+</button>
-                                    </div>
-                                    <div className={styles["meio"]}>
-                                        <h3 className={styles["link"]}>Modificar +</h3>
-                                    </div>
-                                </div>
-                                <div className={styles["card"]}>
-                                    <div className={styles["detailsCard"]}>
-                                        <h3>morangos</h3>
-                                        <h3 style={{ color: "#1B5519" }}>+ R$0,00</h3>
-                                    </div>
-                                    <div className={styles["buttonsCard"]}>
-                                        <button>-</button>
-                                        <div className={styles["number"]}>
-                                            <h3>
-                                                {number}
-                                            </h3>
-                                        </div>
-                                        <button>+</button>
-                                    </div>
-                                    <div className={styles["meio"]}>
-                                        <h3 className={styles["link"]}>Modificar +</h3>
-                                    </div>
-                                </div>
-                                <div className={styles["card"]}>
-                                    <div className={styles["detailsCard"]}>
-                                        <h3>morangos</h3>
-                                        <h3 style={{ color: "#1B5519" }}>+ R$0,00</h3>
-                                    </div>
-                                    <div className={styles["buttonsCard"]}>
-                                        <button>-</button>
-                                        <div className={styles["number"]}>
-                                            <h3>
-                                                {number}
-                                            </h3>
-                                        </div>
-                                        <button>+</button>
-                                    </div>
-                                    <div className={styles["meio"]}>
-                                        <h3 className={styles["link"]}>Modificar +</h3>
-                                    </div>
-                                </div>
-                                <div className={styles["card"]}>
-                                    <div className={styles["detailsCard"]}>
-                                        <h3>morangos</h3>
-                                        <h3 style={{ color: "#1B5519" }}>+ R$0,00</h3>
-                                    </div>
-                                    <div className={styles["buttonsCard"]}>
-                                        <button>-</button>
-                                        <div className={styles["number"]}>
-                                            <h3>
-                                                {number}
-                                            </h3>
-                                        </div>
-                                        <button>+</button>
-                                    </div>
-                                    <div className={styles["meio"]}>
-                                        <h3 className={styles["link"]}>Modificar +</h3>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className={styles["buttonsSpace"]}>
-                                <button className={styles["buttonAdd"]}>Adicionar+</button>
-                            </div>
-                            <div className={styles["check"]}>
-                                <div className={styles["fix"]}>
-                                    <div className={styles["colum"]}>
-                                        <h5>Concluir</h5>
-                                        <img src={check} alt="Check" className={styles["imagens"]} />
-                                    </div>
-                                    <div className={styles["colum"]}>
-                                        <h5>Excluir</h5>
-                                        <img src={cancel} alt="Cancel" className={styles["imagens"]} />
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+  const [nome, setNome] = useState();
+  let id = sessionStorage.getItem("index");
+  const [cardsData, setCardsData] = useState();
+
+  const getCakesData = () => {
+    api
+      .get("/cakes/" + id)
+      .then((response) => {
+        const { data } = response;
+        console.log(data);
+        setCardsData(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const checkIdNull = () => {
+    if (id === null) {
+      window.location.href = "/cardapio";
+    }
+  };
+
+  const funcaoAdicionar = () => {
+    if (cardsData?.adicionais) {
+      if (Array.isArray(cardsData?.adicionais)) {
+        return cardsData?.adicionais.map((data, index) => (
+          <Adicionais key={index} index={index} adicionais={data} />
+        ));
+      }
+    }
+  };
+
+  const salvarEdicao = () => {
+    api
+      .put("/cakes/" + id,{
+        nome: cardsData?.nome,
+        descricao: cardsData?.descricao,
+        preco: cardsData?.preco,
+        adicionais: cardsData?.adicionais,
+      })
+      .then(() => {
+        console.log("oi");
+        sessionStorage.removeItem("index");
+        window.location.href = "/cardapio";
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const deletarBolo = () => {
+    api
+    .delete("/cakes/" + id)
+    .then(() => {
+      sessionStorage.removeItem("index");
+      window.location.href = "/cardapio";
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  };
+
+  useEffect(() => {
+    getCakesData();
+    checkIdNull();
+  }, [id]);
+  return (
+    <div className={styles["corpo"]}>
+      <NavBarAdmin />
+      <div className={styles["editCake"]}>
+        <div className={styles["editLeft"]}>
+          <div className={styles["contentEditLeft"]}>
+            <h3 className={styles["link"]}>Modificar imagem +</h3>
+            <div className={styles["editImage"]}></div>
+          </div>
         </div>
-    );
+        <div className={styles["editRight"]}>
+          <div className={styles["boxEditRight"]}>
+            <div className={styles["contentEditRight"]}>
+              <div className={styles["titleEdit"]}>
+                <h2>{cardsData?.nome}</h2>
+                <h3 className={styles["link"]}>Modificar Nome +</h3>
+              </div>
+              <div className={styles["midEdit"]}>
+                <textarea
+                  placeholder={cardsData?.descricao}
+                  type="text"
+                  className={styles["inputEdit"]}
+                />
+                <h3 className={styles["link"]}>Modificar Descrição +</h3>
+              </div>
+              <div className={styles["titleEdit"]}>
+                <h2 className={styles["priceEdit"]}>R$: {cardsData?.preco}</h2>
+                <h3 className={styles["link"]}>Modificar Preço +</h3>
+              </div>
+              <div className={styles["buttonsEdit"]}>
+                <button className={styles["buttonPurple"]}>1,0kg</button>
+                <button className={styles["buttonPurple"]}>1,0kg</button>
+                <button className={styles["buttonPurple"]}>1,0kg</button>
+                <button className={styles["buttonWhite"]}>+</button>
+              </div>
+              <div className={styles["tabelaEdit"]}>
+                {funcaoAdicionar()}
+              </div>
+              <div className={styles["buttonsSpace"]}>
+                <button className={styles["buttonAdd"]}>Adicionar+</button>
+              </div>
+              <div className={styles["check"]}>
+                <div className={styles["fix"]}>
+                  <div className={styles["colum"]} onClick={salvarEdicao()}>
+                    <h5>Concluir</h5>
+                    <img
+                      src={check}
+                      alt="Check"
+                      className={styles["imagens"]}
+                    />
+                  </div>
+                  <div className={styles["colum"]} onClick={deletarBolo()}>
+                    <h5>Excluir</h5>
+                    <img
+                      src={cancel}
+                      alt="Cancel"
+                      className={styles["imagens"]}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default ModificarBolo;
